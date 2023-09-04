cfb9dc62d32869d46c269e75ac2e722f2779818e
NEODC\qinjiahui <qinjiahui@neotrident.com>
2023-09-04 18:16:23 +0800

    ✨ feat: 增加复制文件/文件夹名称功能

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # cmc-component-creater
 快速创建对应的Modal/Drawer文件
+提供右键复制文件/文件夹名称功能
 
 ## LICENSE
 
--- src/extension.ts
+++ src/extension.ts
@@ -9,7 +9,7 @@ export function activate(context: vscode.ExtensionContext) {
         try {
           const folderName = await getFolderName(uri);
           if (folderName) {
-            generateModal(uri, folderName, commond, 'modal');
+            generateModal(uri, folderName, commond, "modal");
           }
         } catch (error) {
           console.log(error);
@@ -23,13 +23,38 @@ export function activate(context: vscode.ExtensionContext) {
         try {
           const folderName = await getFolderName(uri);
           if (folderName) {
-            generateModal(uri, folderName, commond, 'drawer');
+            generateModal(uri, folderName, commond, "drawer");
           }
         } catch (error) {
           console.log(error);
         }
       })
     );
+  });
+  vscode.commands.registerCommand(
+    "copy.fileNameAndSuffix",
+    (uri: vscode.Uri) => {
+      const allName = uri.fsPath.split("\\").pop();
+      if (allName) {
+        vscode.env.clipboard.writeText(allName);
+        vscode.window.showInformationMessage(`${allName}复制成功`);
+      }
+    }
+  );
+  vscode.commands.registerCommand("copy.fileName", (uri: vscode.Uri) => {
+    const allName = uri.fsPath.split("\\").pop();
+    if (allName) {
+      const [name] = allName.split(".");
+      vscode.env.clipboard.writeText(name);
+      vscode.window.showInformationMessage(`${name}复制成功`);
+    }
+  });
+  vscode.commands.registerCommand("copy.folderName", (uri: vscode.Uri) => {
+    const allName = uri.fsPath.split("\\").pop();
+    if (allName) {
+      vscode.env.clipboard.writeText(allName);
+      vscode.window.showInformationMessage(`${allName}复制成功`);
+    }
   });
 }
 
